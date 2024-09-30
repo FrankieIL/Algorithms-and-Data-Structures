656e1385d5c9b6a5f523bb7990e12b32d574f4d8
FrankieIL <frankie@drakey.co.uk>
2024-09-30 16:39:18 +0100

    First commit, understanding Git.

---
+++ src/ADS/Week1/Practical1/casting.java
@@ -0,0 +1,11 @@
+package ADS.Week1.Practical1;
+
+public class casting {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
